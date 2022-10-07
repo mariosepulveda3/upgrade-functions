230a0a5ace3a090ba7022b96fff87f9511f87d67
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-07 23:26:21 +0200

    DONE, functions

--- iteración-6.js
+++ iteración-6.js
@@ -29,7 +29,7 @@ const duplicates = [
        for (let j = i + 1; j < array.length; j++) {
          if (array[i] === array[j]) {
            array.splice (j, 1);
-          j--;
+          //j--;
          }
        }
      }
